5b44b900070159d3b537cd0fcbc3f5ede98236ee
Rafael Melo <rafahdiaz18@gmail.com>
2026-07-19 15:58:25 -0300

    Implementado MovieResponseDTO mapeamento

--- TMDBAPI/DTOs/MovieDTO.cs
+++ TMDBAPI/DTOs/MovieDTO.cs
@@ -2,10 +2,10 @@ namespace TMDBAPI.DTOs
 {
     public class MovieDTO
     {
-        public string Title { get; set; }
-        public string Overview { get; set; }
-        public string PosterPath { get; set; }
-        public string ReleaseDate { get; set; }
+        public string Title { get; set; } = "";
+        public string Overview { get; set; } = "";
+        public string PosterPath { get; set; } = "";
+        public string ReleaseDate { get; set; } = "";
         public double VoteAverage { get; set; }
     }
 }
